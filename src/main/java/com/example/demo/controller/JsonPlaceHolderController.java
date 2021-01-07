78665a0bbf2c6b16cb4a6ca60d9c4ac29462d105
Keaccious <victor_rockall@hotmil.com>
2021-01-06 23:35:08 -0300

    a

--- src/main/java/com/example/demo/controller/JsonPlaceHolderController.java
+++ src/main/java/com/example/demo/controller/JsonPlaceHolderController.java
@@ -1,15 +1,36 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.jsonplaceholder.response.AlbumResponse;
+import com.example.demo.model.jsonplaceholder.response.PostResponse;
+import com.example.demo.model.jsonplaceholder.response.UserResponse;
+import com.example.demo.service.JsonPlaceHolderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/json")
+@RequestMapping("/complete")
 
 public class JsonPlaceHolderController {
 
+    private final JsonPlaceHolderService jsonService;
 
+    @GetMapping ("/post")
+    public List<PostResponse> getCompletePostResponse () {
+        return jsonService.getCompletePost();
+    }
 
+    @GetMapping ("/album")
+    public List<AlbumResponse> getCompleteAlbumResponse (){
+        return jsonService.getCompleteAlbum();
+    }
+
+    @GetMapping ("/user")
+    public List<UserResponse> getCompleteUserResponse(){
+        return jsonService.getcompleteUser();
+    }
 }
--- src/main/java/com/example/demo/service/JsonPlaceHolderService.java
+++ src/main/java/com/example/demo/service/JsonPlaceHolderService.java
@@ -1,8 +1,16 @@
 package com.example.demo.service;
 
+import com.example.demo.model.jsonplaceholder.*;
+import com.example.demo.model.jsonplaceholder.response.AlbumResponse;
+import com.example.demo.model.jsonplaceholder.response.PostResponse;
+import com.example.demo.model.jsonplaceholder.response.UserResponse;
+import com.example.demo.model.jsonplaceholder.user.User;
 import com.example.demo.repository.JsonPlaceHolderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -11,4 +19,70 @@ public class JsonPlaceHolderService {
 
     private final JsonPlaceHolderRepository jsonPlaceHolderRepository;
 
+    public List<PostResponse> getCompletePost (){
+        List<Post> posts = jsonPlaceHolderRepository.getPosts();
+        List<Comment> comments = jsonPlaceHolderRepository.getComments();
+        List<PostResponse> responsePost = posts.stream().map(post -> {
+            List<Comment> filteredComments = comments.stream()
+                    .filter(comment -> comment.getPostId().equals(post.getId()))
+                    .collect(Collectors.toList());
+            return PostResponse.builder()
+                    .userId(post.getUserId())
+                    .id(post.getId())
+                    .title(post.getTitle())
+                    .body(post.getBody())
+                    .comments(filteredComments)
+                    .build();
+        }).collect(Collectors.toList());
+        return responsePost;
+    }
+
+    public List<AlbumResponse> getCompleteAlbum(){
+        List<Album> albums = jsonPlaceHolderRepository.getAlbums();
+        List<Photo> photos = jsonPlaceHolderRepository.getPhotos();
+        List<AlbumResponse> responseAlbum = albums.stream().map(album ->{
+            List<Photo> filteredPhotos = photos.stream()
+                    .filter(photo -> photo.getAlbumId().equals(album.getId()))
+                    .collect(Collectors.toList());
+            return AlbumResponse.builder()
+                    .userId(album.getUserId())
+                    .id(album.getId())
+                    .title(album.getTitle())
+                    .photos(filteredPhotos)
+                    .build();
+        }).collect(Collectors.toList());
+        return responseAlbum;
+    };
+
+    public List<UserResponse> getcompleteUser(){
+        List<User> users = jsonPlaceHolderRepository.getUsers();
+        List<Todo> todos = jsonPlaceHolderRepository.getTodos();
+        List<AlbumResponse> albums = getCompleteAlbum();
+        List<PostResponse> posts = getCompletePost();
+        List<UserResponse> responseUser = users.stream().map(user -> {
+            List<Todo> filteredTodo = todos.stream()
+                    .filter(todo -> todo.getUserId().equals(user.getId()))
+                    .collect(Collectors.toList());
+            List<AlbumResponse> filteredAlbum = albums.stream()
+                    .filter(albumResponse -> albumResponse.getUserId().equals(user.getId()))
+                    .collect(Collectors.toList());
+            List<PostResponse> filteredPost = posts.stream()
+                    .filter(postResponse -> postResponse.getUserId().equals(user.getId()))
+                    .collect(Collectors.toList());
+            return UserResponse.builder()
+                    .id(user.getId())
+                    .name(user.getName())
+                    .username(user.getUsername())
+                    .email(user.getEmail())
+                    .address(user.getAddress())
+                    .phone(user.getPhone())
+                    .website(user.getWebsite())
+                    .company(user.getCompany())
+                    .posts(filteredPost)
+                    .albums(filteredAlbum)
+                    .todos(filteredTodo)
+                    .build();
+        }).collect(Collectors.toList());
+        return responseUser;
+    };
 }
